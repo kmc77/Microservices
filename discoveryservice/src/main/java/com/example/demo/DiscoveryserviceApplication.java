37d1e1621473606e2fba4ccf61c18a88c298b10b
kmc77 <k_minchoi@naver.com>
2023-09-22 11:33:10 +0900

    1. discoveryservice

---
+++ discoveryservice/src/main/java/com/example/demo/DiscoveryserviceApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer //유레카 서버로 작동하기 위해 사용
+public class DiscoveryserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryserviceApplication.class, args);
+	}
+
+}

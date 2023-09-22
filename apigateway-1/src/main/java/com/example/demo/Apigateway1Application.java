d2021153cccc7d8271a30b5d6e8c319c820a39a4
kmc77 <k_minchoi@naver.com>
2023-09-22 14:33:50 +0900

    apigateway-1

---
+++ apigateway-1/src/main/java/com/example/demo/Apigateway1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apigateway1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apigateway1Application.class, args);
+	}
+
+}
---
+++ apigateway-1/src/main/java/com/example/demo/SecondController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SecondController {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome to SecondService";
+	}
+
+}
